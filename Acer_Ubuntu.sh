11a09c92750fb4602e9f43a7d29517cf38f976d0
Timmy Chan <Timmeh.Chan@gmail.com>
2022-03-23 18:35:05 -0700

    Wed 23 Mar 2022 06:35:05 PM PDT

--- Acer_Ubuntu.sh
+++ Acer_Ubuntu.sh
@@ -78,7 +78,7 @@ snap install gh
 # https://www.gnu.org/software/sed/manual/sed.html
 # https://ubuntuforums.org/showthread.php?t=2450981
 # https://sciactive.com/2020/12/04/how-to-install-ubuntu-on-acer-spin-5-sp513-54n-for-the-perfect-linux-2-in-1/
-sed 's/GRUB_CMDLINE_LINUX_DEFAULT=.*/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash i8042.nopnp=1 pci=nocrs"/' /etc/default/grub
+sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT=.*/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash i8042.nopnp=1 pci=nocrs"/' /etc/default/grub
 update-grub
 
 apt -qy update 

87874a06ff741ea11d5a21a434b5232d8e563989
Timmy Chan <Timmeh.Chan@gmail.com>
2022-04-06 22:53:38 -0700

    I ono why but now I have to sudo to add and commit manually...?

--- Git_Push_Now.sh
+++ Git_Push_Now.sh
@@ -3,10 +3,7 @@
 function gitpushnow() {
   now=`date`
   echo "Last run: ${now}" > $1/status.txt
-  echo "By $USER" >> $1/status.txt
-  echo "tlp-stat output:" >> $1/status.txt
-  echo "##########" >> $1/status.txt
-  tlp-stat >> $1/status.txt
+  tlp-stat >> $1/tlp_stat.log
   ##### Pushing to Git #####
   git -C $1 add .
   git -C $1 commit -m "${now}"
